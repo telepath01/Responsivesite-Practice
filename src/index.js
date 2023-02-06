25fdac2637563b83fe64871b78f876ff3e209ff4
Paul Rodriguez <paul.rodriguez03@gmail.com>
2023-02-06 14:39:57 -0500

    working on animation
    working on the animation tutorial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-"use strict";
+'use strict';
+const triggerDiv = document.querySelector('.trigger');
+triggerDiv.addEventListener('click', (event) => {
+  if (triggerDiv.className === 'trigger') {
+    triggerDiv.className = 'trigger clicked';
+  } else triggerDiv.className = 'trigger';
+});
