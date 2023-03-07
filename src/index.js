4bc17d1e5e19bb730515ddcee29d8236c9b7ce29
Paul Rodriguez <paul.rodriguez03@gmail.com>
2023-03-07 17:10:17 -0500

    working with json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,58 @@
-'use strict';
-const triggerDiv = document.querySelector('.trigger');
-triggerDiv.addEventListener('click', (event) => {
-  if (triggerDiv.className === 'trigger') {
-    triggerDiv.className = 'trigger clicked';
-  } else triggerDiv.className = 'trigger';
-});
+async function populate() {
+  const requestURL =
+    'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+  const request = new Request(requestURL);
+
+  const respons = await fetch(request);
+  const superHeroes = await respons.json();
+
+  populateHeader(superHeroes);
+  populateHeroes(superHeroes);
+}
+
+function populateHeader(obj) {
+  const header = document.querySelector('header');
+  const myH1 = document.createElement('h1');
+  myH1.textContent = obj.squadName;
+  header.appendChild(myH1);
+
+  const myPara = document.createElement('p');
+  myPara.textContent = `Hometown: ${obj.homeTown} // Formed: ${obj.formed}`;
+  header.appendChild(myPara);
+}
+
+function populateHeroes(obj) {
+  const section = document.querySelector('section');
+  const heroes = obj.members;
+
+  for (const hero of heroes) {
+    const myArticle = document.createElement('article');
+    const myH2 = document.createElement('h2');
+    const myPara1 = document.createElement('p');
+    const myPara2 = document.createElement('p');
+    const myPara3 = document.createElement('p');
+    const myList = document.createElement('ul');
+
+    myH2.textContent = hero.name;
+    myPara1.textContent = `Secret identity: ${hero.secretIdentity}`;
+    myPara2.textContent = `Age: ${hero.age}`;
+    myPara3.textContent = 'Superpowers:';
+
+    const Superpowers = hero.powers;
+    for (const power of Superpowers) {
+      const listItem = document.createElement('li');
+      listItem.textContent = power;
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
+
+populate();
