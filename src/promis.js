0875902dcc525606b788cf65b0e346aaf65ca5e9
Paul Rodriguez <paul.rodriguez03@gmail.com>
2023-03-10 21:33:12 -0500

    working on things

--- src/promis.js
+++ src/promis.js
@@ -1,13 +1,12 @@
 'use strict';
-new Promise((resolve, reject) => {
-  reject('Nope');
-})
-  .then(() => {
-    console.log('success');
+const img = document.querySelector('img');
+fetch(
+  'https://api.giphy.com/v1/gifs/translate?api_key=7mAve8bXCNMOeUNM0853doI6b6ZXPk6R&s=cats',
+  { mode: 'cors' }
+)
+  .then(function (response) {
+    return response.json();
   })
-  .catch(() => {
-    console.log('fail');
-  })
-  .finally((res) => {
-    console.log('finally');
+  .then(function (response) {
+    img.src = response.data.images.original.url;
   });
