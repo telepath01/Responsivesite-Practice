e9dcb1614f6b3339b9a8de1b17a620eb1d93b86f
Paul Rodriguez <paul.rodriguez03@gmail.com>
2023-03-20 17:39:00 -0400

    working on recursion

--- src/promis.js
+++ src/promis.js
@@ -1,12 +1,17 @@
 'use strict';
-const img = document.querySelector('img');
 
-async function getCats() {
-  const response = await fetch(
-    'https://api.giphy.com/v1/gifs/translate?api_key=7mAve8bXCNMOeUNM0853doI6b6ZXPk6R&s=cats',
-    { mode: 'cors' }
-  );
-  const catData = await response.json();
-  img.src = catData.data.images.original.url;
+function countDown(n) {
+  for (let i = n; i > 0; i--) {
+    console.log(i);
+  }
+  console.log('done');
 }
-getCats();
+
+function countDownRecursive(n) {
+  if (n <= 0) {
+    console.log('done');
+    return;
+  }
+  console.log(n);
+  countDownRecursive(n - 1);
+}
